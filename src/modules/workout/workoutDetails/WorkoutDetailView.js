4f9aa7651c1050425e7f326ace098ccf534e6a3a
AndrewZurn <awzurn@gmail.com>
2016-09-06 22:29:23 -0500

    Adds Reps/Weight input field, picker button for minutes/seconds based tasks.

--- src/modules/workout/workoutDetails/WorkoutDetailView.js
+++ src/modules/workout/workoutDetails/WorkoutDetailView.js
@@ -1,19 +1,21 @@
 import React, {PropTypes} from 'react';
 import {
     Dimensions,
-    View,
     StyleSheet,
     ScrollView,
     TextInput,
-    Text
+    Text,
+    View
 } from 'react-native';
 import {Card, Button} from 'react-native-material-design';
 import Colors from '../../../utils/colors';
+import Picker from 'react-native-picker';
 import RadioButton from 'react-native-radio-button';
 import * as WorkoutState from '../WorkoutState';
 import * as WorkoutUtils from '../../../utils/workoutUtils';
 
 const width = Dimensions.get('window').width;
+const EXERCISE_OPTION_INPUT_WIDTH = 0.87;
 
 const WorkoutDetailView = React.createClass({
   propTypes: {
@@ -40,7 +42,9 @@ const WorkoutDetailView = React.createClass({
       return {
         exerciseOptionId: exerciseOption.id,
         alternativeIsSelected: false,
-        altExerciseOptionId
+        altExerciseOptionId,
+        displayValue: null,
+        value: null
       };
     });
   },
@@ -55,17 +59,17 @@ const WorkoutDetailView = React.createClass({
    * Will find the exerciseOption for the card that was selected, and update which of the option's is
    * in the selected position.
    *
-   * @param exerciseOptionId The exercise option/parent of alternative option whose radio button was selected.
-   * @param alternativeIsSelected Flag for whether it was the alternative option that was selected or not.
+   * @param exerciseOptionId {uuid} The exercise option/parent of alternative option whose radio button was selected.
+   * @param alternativeIsSelected {boolean} Flag for whether it was the alternative option that was selected or not.
    * @private
    */
   _updateSelectedExerciseOption(exerciseOptionId, alternativeIsSelected) {
     let updatedSelectedExercises = this.state.selectedExercises.map(option => {
       if (option.exerciseOptionId === exerciseOptionId) {
         return {
+          ...option,
           exerciseOptionId,
-          alternativeIsSelected: alternativeIsSelected,
-          altExerciseOptionId: option.altExerciseOptionId
+          alternativeIsSelected
         };
       } else {
         return option;
@@ -85,14 +89,18 @@ const WorkoutDetailView = React.createClass({
     }
   },
 
+  _getExerciseOptionDescriptionText(exerciseOption, optionId) {
+    return exerciseOption.description
+        ? <Text style={styles.text} key={'desc_' + optionId}>{exerciseOption.description}</Text> : null;
+  },
+
   _createAlternativeOptionCard(exerciseOption, optionId) {
     let alternativeOptionView;
     if (exerciseOption.alternativeExerciseOption) {
       const alternativeOption = exerciseOption.alternativeExerciseOption;
       const altId = alternativeOption.id;
 
-      let alternativeOptionDescText = this._getExerciseOptionDescription(alternativeOption, altId);
-
+      let alternativeOptionDescText = this._getExerciseOptionDescriptionText(alternativeOption, altId);
       let amount = this._getExerciseOptionAmount(alternativeOption);
 
       alternativeOptionView = (
@@ -122,11 +130,91 @@ const WorkoutDetailView = React.createClass({
     return alternativeOptionView;
   },
 
-  _getExerciseOptionDescription(exerciseOption, optionId) {
-    if (exerciseOption.description) {
-      return <Text style={styles.text} key={'desc_' + optionId}>{exerciseOption.description}</Text>;
+  _getExerciseOption(exerciseOptionId) {
+    return this.state.selectedExercises.find(option => option.exerciseOptionId === exerciseOptionId);
+  },
+
+  _getExerciseOptionDisplayValue(exerciseOptionId) {
+    let foundOption = this._getExerciseOption(exerciseOptionId);
+    return foundOption && foundOption.displayValue ? foundOption.displayValue : '';
+  },
+
+  _updateExerciseOptionValue(exerciseOption, value, displayValueAppender, isEndEditing) {
+    let updatedExerciseOptions = this.state.selectedExercises.map(option => {
+      if (option.exerciseOptionId === exerciseOption.id) {
+        let updatedDisplayValue;
+        if (!isEndEditing) { // is still changing the value
+          updatedDisplayValue = value;
+        } else if (value && value.length > 0) { // is done changing the value
+          updatedDisplayValue = `${value} ${displayValueAppender}`;
+        } else { // there is no value to display (value is also null or empty)
+          updatedDisplayValue = null;
+        }
+
+        return {
+          exerciseOptionId: exerciseOption.id,
+          alternativeIsSelected: option.alternativeIsSelected,
+          altExerciseOptionId: option.altExerciseOptionId,
+          displayValue: updatedDisplayValue,
+          value
+        };
+      } else {
+        return option;
+      }
+    });
+
+    this.setState({...this.state, selectedExercises: updatedExerciseOptions});
+  },
+
+  _getInputComponent(exerciseOption) {
+    if (exerciseOption.inputType === 'time') {
+      return (
+          <Button
+              text='Enter Timed Results'
+              raised={true}
+              overrides={styles.timedResultsButton}
+              onPress={() => {
+                if (exerciseOption.inputType === 'time' && this.picker) {
+                  this.picker.toggle();
+                }
+              }}
+          />
+      );
+    } else if (exerciseOption.inputType === 'NA') {
+      return null;
+    } else { // numeric
+      let resultsDisplayFieldName;
+      let resultsDisplayFieldAppender; // should be applied to the value of the field when entered/present (and not being edited)
+      if (exerciseOption.type.toUpperCase() === 'HEAVY') {
+        resultsDisplayFieldName = 'Weight';
+        resultsDisplayFieldAppender = 'lbs';
+      } else {
+        resultsDisplayFieldName = 'Repetitions';
+        resultsDisplayFieldAppender = 'Reps';
+      }
+
+      let resultsPlaceholderText = `Enter ${resultsDisplayFieldName}`;
+      return (
+          <View style={styles.textInputParent}>
+            <TextInput
+                style={[styles.textInput]}
+                keyboardType='numeric'
+                // different displayValue when done editing to display something like (ie '50 Reps' or '200 lbs')
+                onEndEditing={() => { // done dditing, add the appender to use as the displayValue
+                  let value = this._getExerciseOption(exerciseOption.id).value;
+                  this._updateExerciseOptionValue(exerciseOption, value, resultsDisplayFieldAppender, true);
+                }}
+                onFocus={() => { // reset the text box to use just the value (and not the displayValue)
+                  let value = this._getExerciseOption(exerciseOption.id).value
+                  this._updateExerciseOptionValue(exerciseOption, value, value, false);
+                }}
+                onChangeText={(text) => this._updateExerciseOptionValue(exerciseOption, text, null, false) }
+                placeholder={resultsPlaceholderText}
+                placeholderTextColor={Colors.spacGold}
+                value={this._getExerciseOptionDisplayValue(exerciseOption.id)}/>
+          </View>
+      );
     }
-    return null;
   },
 
   render() {
@@ -146,9 +234,13 @@ const WorkoutDetailView = React.createClass({
       let alternativeOptionView = this._createAlternativeOptionCard(exerciseOption, optionId);
 
       // get the optional description for display if found
-      let exerciseOptionDescriptionText = this._getExerciseOptionDescription(exerciseOption, optionId);
+      let exerciseOptionDescriptionText = this._getExerciseOptionDescriptionText(exerciseOption, optionId);
 
+      // get the amount (targetAmount or duration) of the exercise option.
       let amount = this._getExerciseOptionAmount(exerciseOption);
+
+      // get the input type for the display type of the exercise option
+      let inputComponent = this._getInputComponent(exerciseOption);
 
       return (
           <Card style={styles.card} key={'card_' + optionId}>
@@ -174,18 +266,7 @@ const WorkoutDetailView = React.createClass({
               </View>
               {alternativeOptionView}
 
-              <View style={{flex: 1, alignItems: 'flex-end'}}>
-                <View style={styles.textInputParent}>
-                  <TextInput
-                      style={[styles.textInput]}
-                      keyboardType='numeric'
-                      onEndEditing={() => console.log('or im here') }
-                      onChangeText={(text) => console.log('im here') }
-                      placeholder='Enter Results'
-                      placeholderTextColor={Colors.spacGold}
-                      value={0}/>
-                </View>
-              </View>
+              {inputComponent}
             </Card.Body>
           </Card>
       );
@@ -205,8 +286,49 @@ const WorkoutDetailView = React.createClass({
                       style={styles.scrollView}>
             {exerciseOptionsCards}
           </ScrollView>
+
+          // picker for timed based results (displays minutes and seconds)
+          <Picker
+              ref={picker => {
+                this.picker = picker;
+                return;
+              }}
+              style={{
+                height: 280
+              }}
+              pickerElevation={100}
+              pickerTitle={'Completed In'}
+              pickerCancelBtnText={'Close'}
+              pickerBtnText={'Save'}
+              showMask={true}
+              pickerToolBarStyle={{backgroundColor: Colors.spacMediumGray, height: 35}}
+              pickerTitleStyle={styles.altTitle}
+              pickerData={[this._getMinutesArray(), this._getSecondsArray()]} //picker`s value List
+              selectedValue={['0 Minutes', '1 Seconds']} //default to be selected value
+              onPickerDone={(value) => console.log(`My picked value is ${value}`)}
+          />
         </View>
     );
+  },
+
+  _getMinutesArray() {
+    return [...Array(31).keys()].map(i => {
+      if (i % 10 === 0) {
+        return `${i} Minutes`;
+      } else {
+        return `${i}`;
+      }
+    });
+  },
+
+  _getSecondsArray() {
+    return [...Array(60).keys()].map(i => {
+      if (i % 10 === 0) {
+        return `${i + 1} Seconds`;
+      } else {
+        return `${i + 1}`;
+      }
+    });
   }
 });
 
@@ -245,17 +367,25 @@ const styles = StyleSheet.create({
     fontFamily: Colors.textStyle
   },
   textInput: {
-    textAlign: 'center',
     height: 35,
-    width: width * 0.38,
+    width: width * EXERCISE_OPTION_INPUT_WIDTH,
+    textAlign: 'center',
     backgroundColor: Colors.spacLightGray,
     color: Colors.spacGold,
     fontFamily: Colors.textStyle
   },
   textInputParent: {
-    width: width * 0.38,
+    height: 35,
+    width: width * EXERCISE_OPTION_INPUT_WIDTH,
+    marginTop: 10,
+    justifyContent: 'center',
     borderBottomColor: Colors.spacGold,
     borderBottomWidth: 1
+  },
+  timedResultsButton: {
+    textColor: Colors.spacGold,
+    backgroundColor: Colors.spacMediumGray,
+    rippleColor: Colors.spacLightGray
   },
   scrollView: {
     flex: 1
